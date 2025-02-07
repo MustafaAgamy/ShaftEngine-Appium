62611f2bd15493d3734be0feb8f56cbd97b54dd0
MustafaAgamy <mostafaagamy1995@gmail.com>
2025-02-07 16:17:20 +0200

    Add Comments

--- src/test/java/AppiumSampleTests.java
+++ src/test/java/AppiumSampleTests.java
@@ -19,18 +19,26 @@ public class AppiumSampleTests {
                 .verifyColorFieldExists();
     }
 
+    //Before Class To Set up the Service And Properties
     @BeforeClass
     public void beforeClassSetUp() {
         service = new AppiumServiceBuilder()
+                //The Port for the Service, on must match it with the executionAddress port
                 .usingPort(Integer.parseInt(System.getProperty("port")))
+                //The IP Address for the Service, on must match it with the executionAddress Address
                 .withIPAddress(System.getProperty("ipAddress"))
+                //To Add Special Arguments to the running Appium Service
+                //This command will run the service on UiAutomator2
                 .withArgument(() -> System.getProperty("commandName"), AutomationName.ANDROID_UIAUTOMATOR2.toLowerCase())
                 .build();
         service.start();
 
         System.setProperty("mobile_autoGrantPermissions", "true");
 
-        // common attributes
+        //common attributes, set them according to your needs
+        //Note that you can also add the value to the custom.properties file, for more info visit:
+        //https://shafthq.github.io/docs/Properties/PropertiesList#mobile
+        //https://shafthq.github.io/docs/Properties/PropertiesList#platform
         SHAFT.Properties.platform.set().targetPlatform(Platform.ANDROID.name());
         SHAFT.Properties.mobile.set().automationName(AutomationName.ANDROID_UIAUTOMATOR2);
         SHAFT.Properties.platform.set().executionAddress("127.0.0.1:4723");
@@ -38,17 +46,19 @@ public class AppiumSampleTests {
         SHAFT.Properties.mobile.set().deviceName("Medium Phone API 35");
     }
 
+    //Before Method To Set up the SHAFT driver
     @BeforeMethod
     public void beforeMethodSetUp() {
         driver = new SHAFT.GUI.WebDriver();
     }
 
-
+    //After Method To Quit the Running Driver instance
     @AfterMethod
     public void afterMethodTearDown() {
         driver.quit();
     }
 
+    //After Method To Quit the Running Service instance
     @AfterClass
     public void afterClassTearDown() {
         service.close();
